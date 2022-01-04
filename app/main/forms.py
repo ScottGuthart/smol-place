16c2d6a8edf9a968d91dec47ca5a2f66391651c5
Scott Guthart <seguthart@gmail.com>
2022-01-03 22:13:26 -0500

    valid url example

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,22 +1,36 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, URL, Regexp, Length
+from wtforms.validators import (
+    ValidationError,
+    DataRequired,
+    URL,
+    Regexp,
+    Length,
+)
 from app.models import Site
 
 
 class AddSiteForm(FlaskForm):
-    url = StringField("url", validators=[URL(), DataRequired()])
+    url = StringField(
+        "url",
+        validators=[
+            URL(message="Please enter a valid URL (https://example.com)"),
+            DataRequired(),
+        ],
+    )
     alias = StringField(
         "alias",
         validators=[
             DataRequired(),
-            Regexp("^[a-zA-Z0123456789\\-_.~]*$", message="Invalid characters"),
+            Regexp(
+                "^[a-zA-Z0123456789\\-_.~]*$", message="Invalid characters"
+            ),
             Length(min=1, max=200),
         ],
     )
-    submit = SubmitField('Add Alias')
+    submit = SubmitField("Add Alias")
 
     def validate_alias(self, alias):
         site = Site.query.filter(Site.alias.ilike(self.alias.data)).first()
-        if self.alias.data.lower() == 'add' or site is not None:
+        if self.alias.data.lower() == "add" or site is not None:
             raise ValidationError("Please use a different alias.")
